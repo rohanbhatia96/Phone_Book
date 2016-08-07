72687b5447d0f1bd3dffa21b896aa6eb24d28018
Rohan Bhatia <rohanbhatia96@outlook.com>
2016-08-07 22:00:48 +0530

    Commit 2

--- app/src/main/java/bhatia/rohan/phonebook/MainActivity.java
+++ app/src/main/java/bhatia/rohan/phonebook/MainActivity.java
@@ -1,13 +1,19 @@
 package bhatia.rohan.phonebook;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton I1;
+    SQLiteDatabase sq;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sq=openOrCreateDatabase("mydb",SQLiteDatabase.CREATE_IF_NECESSARY,null);
+        sq.execSQL("create table if not exists phonebook(first text,last text,email text, phone integer)");
     }
 }
